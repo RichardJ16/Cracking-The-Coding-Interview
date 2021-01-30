b7bc4f4a6701a310a9383bc857d5291a4a910639
Richard <RMJohnson1616@gmail.com>
2021-01-30 11:20:46 -0600

    Matrix Rotation

---
+++ Chapter-1/Q1_7.py
@@ -0,0 +1,28 @@
+# rotate matrix
+
+def rotateMatrix(m1):
+
+    for i in range(len(m1)):
+    
+        for j in range(i, len(m1)):
+        
+            m1[i][j], m1[j][i] = m1[j][i], m1[i][j]
+            
+
+    for k in range(len(m1)):
+    
+        for l in range(len(m1[k]) // 2):
+        
+            temp = len(m1[l]) - 1 - l
+            
+            m1[k][l], m1[k][temp] = m1[k][temp], m1[k][l]
+             
+    print(m1)
+    
+    
+    
+m1 = [[1,2,3],[4,5,6],[7,8,9]]
+rotateMatrix(m1)
+# [1 2 3]
+# [4 5 6]
+# [7 8 9]
