e839225beaf374c8fdeff141d44f7d8bcc939e43
Richard <RMJohnson1616@gmail.com>
2021-02-04 15:21:24 -0600

    Generic data types and problem set up

---
+++ Chapter-2/Q2_1.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+
+/* Question 2.1
+Remove Duplicates from an unsorted linked list
+*/
+
+struct GENERIC
+{
+    enum {
+      typUndefined,
+      typInt,           // 1
+      typUint,
+      typString,
+      typByteString,
+      typLong,          // 5
+      typFloat,
+      typDouble,
+    } iType;
+
+    union
+    {
+        int i;
+        unsigned int u;
+        char* s;
+        long l;
+		
+    } value;
+};
+
+typedef struct {
+    generic type;
+    union {
+        int a; 
+        float b; 
+        char c;
+    } my_union;
+} my_struct;
+
+void problemSet() {
+	
+	LinkedList list = new_LinkedList();
+	printf("new list =");
+	toString(list);
+	
+	// Test 1
+	
+	addToEnd(list, "a");
+	addToEnd(list, "b");
+	addToEnd(list, "a");
+	
+	printf("new list =");
+	toString(list);
+	
+	removeDupe(list)
+}
+LinkedList removeDupe(LinkedList list) {
+	
+	
+	
+	return list;
+}
+
+
+
+int main(int argc, char *argv[]) {
+	
+	problemSet();
+	
+}
